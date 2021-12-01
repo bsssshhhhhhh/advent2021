f8a0b65f7f71cbe5a3ad12a69f9db32628cba5df
Brian Simon <bsssshhhhhhh@gmail.com>
2021-12-01 08:21:00 -0500

    day 01

---
+++ day 01/part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using AdventCommon;
+using System.Linq;
+
+var input = ConsoleHelpers
+    .ReadInput()
+    .Select(i => int.Parse(i))
+    .ToArray();
+
+var gt = 0;
+for (var i = 1; i < input.Length; i++)
+{
+    if (input[i] > input[i - 1])
+    {
+        gt++;
+    }
+}
+
+Console.WriteLine(gt);
---
+++ day 01/part2/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventCommon;
+using System.Linq;
+
+var input = ConsoleHelpers
+    .ReadInput()
+    .Select(i => int.Parse(i))
+    .ToArray();
+
+var gt = 0;
+
+var getWindow = (int i) => input.Skip(i).Take(3).Sum();
+
+for (var i = 0; i < input.Length; i++)
+{
+    if (getWindow(i + 1) > getWindow(i))
+    {
+        gt++;
+    }
+}
+
+Console.WriteLine(gt);
