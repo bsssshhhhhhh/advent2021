50ac0898323a8fcbe226ab66071b0fabb8dcb135
Brian Simon <bsssshhhhhhh@gmail.com>
2021-12-02 07:32:08 -0500

    day 02

---
+++ day 02/part1/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventCommon;
+var lines = ConsoleHelpers.ReadInput();
+
+int x = 0;
+int y = 0;
+
+
+for (var i = 0; i < lines.Length; i++)
+{
+    var parts = lines[i].Split(" ");
+    var distance = int.Parse(parts[1]);
+
+    switch (parts[0])
+    {
+        case "forward": x += distance; break;
+        case "down": y += distance; break;
+        case "up": y -= distance; break;
+    }
+}
+
+Console.WriteLine(x * y);
---
+++ day 02/part2/Program.cs
@@ -0,0 +1,26 @@
+﻿using AdventCommon;
+var lines = ConsoleHelpers.ReadInput();
+
+int x = 0;
+int y = 0;
+int aim = 0;
+
+
+for (var i = 0; i < lines.Length; i++)
+{
+    var parts = lines[i].Split(" ");
+    var distance = int.Parse(parts[1]);
+
+    switch (parts[0])
+    {
+        case "forward": {
+          x += distance;
+          y += aim * distance;
+          break;
+        }
+        case "down": aim += distance; break;
+        case "up": aim -= distance; break;
+    }
+}
+
+Console.WriteLine(x * y);
