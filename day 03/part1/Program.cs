276be088293715bd53d828255a2d023b741c70ef
Brian Simon <bsssshhhhhhh@gmail.com>
2021-12-03 08:53:29 -0500

    day 3 part 1

---
+++ day 03/part1/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventCommon;
+var lines = ConsoleHelpers.ReadInput();
+
+var len = lines[0].Length;
+var gammaRateString = "";
+
+
+for (var i = 0; i < len; i++) {
+    gammaRateString += Math.Round(lines.Select(x => x[i] == '0' ? 0 : 1).Average());
+}
+
+var gammaRate = Convert.ToInt32(gammaRateString, 2);
+var epsilonRate = ~gammaRate & ((1 << len) - 1);
+
+Console.WriteLine(gammaRate * epsilonRate);
