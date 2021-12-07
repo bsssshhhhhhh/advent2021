a048c5218dcec9bd6d80fd9519fcf9f04d0d153f
Brian Simon <bsssshhhhhhh@gmail.com>
2021-12-07 08:47:22 -0500

    day 07

---
+++ day 07/part2/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventCommon;
+var lines = ConsoleHelpers.ReadInput();
+
+var crabs = lines[0].Split(",").Select(int.Parse);
+
+var fuel = Enumerable
+    .Range(0, crabs.Max())
+    .Select(difference => crabs
+        .Select(crab => Math.Abs(crab - difference))
+        .Select(fuel => (1 + fuel) * (fuel / 2m))
+    )
+    .Select(fuel => fuel.Sum())
+    .Min();
+
+Console.WriteLine(Convert.ToInt32(fuel));
